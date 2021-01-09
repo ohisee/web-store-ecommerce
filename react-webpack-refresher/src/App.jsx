8e44eb611cbc5b35e6319504110643081ba459d0
ohisee <breakosin@hotmail.com>
2021-01-09 01:19:32 -0800

    Build React app by using webpack

---
+++ react-webpack-refresher/src/App.jsx
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview app component
+ */
+import React from "react";
+import "./App.css";
+
+/** @type {React.FC<{}>} */
+const App = () => {
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="title">Our React is working</h1>
+      </div>
+    </div>
+  )
+};
+
+export default App;
---
+++ react-webpack-refresher/src/index.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview index
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
