1c4115dccfed2799695d0f2d61edb243fc2fe77f
ohisee <breakosin@hotmail.com>
2021-03-10 00:35:47 -0800

    Create a React 17 version of the project with service worker

---
+++ web-store-019/src/components/error-boundary/error-boundarty.styles.tsx
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview error boundary styles  
+ */
+import styled from "styled-components";
+
+export const ErrorImageOverlay = styled.div`
+  height: 60vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface Props {
+  imageUrl: string,
+}
+export const ErrorImageContainer = styled.div<Props>`
+  display: inline-block;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+  background-size: cover;
+  background-position: center;
+  width: 40vh;
+  height: 40vh;
+`;
+
+export const ErrorImageText = styled.h2`
+  font-size: 28px;
+  color: #2f8e89;
+`;
